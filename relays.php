0f7a1dffb1df764b6f7f5f6d4827e84566ca510c
Mastro <7125332+Mas7ro@users.noreply.github.com>
2018-10-18 17:03:26 +0200

    Update relays.php

--- relays.php
+++ relays.php
@@ -6,7 +6,8 @@
 </head>
 <body>
 	<?php
-	function zerificabinario( $rel ) {
+	function zerificabinario( $rel ) 
+	{ //function to add a 0 to the beginning of the binary
 		$caratteriBin = strlen( $rel );
 		$diff = 8 - $caratteriBin;
 		if ( $diff > 0 ) {
@@ -17,7 +18,8 @@
 		return ( $rel );
 	}
 
-	function caricastatorelays() {
+	function caricastatorelays() 
+	{ //function to load relays status
 		global $arrRelays;
 		$statorele = shell_exec( '/usr/sbin/i2cget -y 1 0x23' );
 		$relays = zerificabinario( base_convert( $statorele, 16, 2 ) );
@@ -26,21 +28,24 @@
 	}
 	echo( "<br>" );
 	caricastatorelays();
-	if ( isset( $_POST[ "ID" ] ) ) {
+	if ( isset( $_POST[ "ID" ] ) ) 
+	{
 		$toggle = $_POST[ "ID" ];
 		$toggle = ( substr( $toggle, -1, 1 ) ) - 1;
 		$changedValue = ( 0 == $arrRelays[ $toggle ] ) ? '1' : '0';
 		$arrRelaysToggle = array_replace( [], $arrRelays, [ $toggle => $changedValue ] );
 		$newrelaystatus = implode( $arrRelaysToggle );
 		$newrelaystatus = base_convert( $newrelaystatus, 2, 16 );
-		shell_exec( 'i2cset -y 1 0x23 0x' . $newrelaystatus );
+		shell_exec( 'i2cset -y 1 0x23 0x' . $newrelaystatus ); 
+		// !!!!! change 0x23 with I2C addres of your relays board
 		caricastatorelays();
-		header( 'Location: ' . $_SERVER[ 'PHP_SELF' ] );
+		header( 'Location: ' . $_SERVER[ 'PHP_SELF' ] ); // make a redirect to empty post data. in case of reload of the page i don't wont to re-toggle last action
 	}
 	?>
 	<br>
 	<?php
-	foreach ( $arrRelays as $key => $valore ) {
+	foreach ( $arrRelays as $key => $valore ) 
+	{   //create a form for every relay and his button
 		echo( "<p><form  method='post' style='width: 300px;margin: 0px;'>" );
 		$color = ( 0 == $valore ) ? '#f009' : '#fff';
 		echo( '<input type="submit" name="ID" id="' . $key . '" style="background-color:' . $color . '" value="Relay ' . ( $key + 1 ) . '">' );
